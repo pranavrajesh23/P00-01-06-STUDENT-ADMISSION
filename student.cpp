a02d0e5a02d70d17bfc7e0454fe45e117c8edffc
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-16 12:25:37 +0530

    main and registeration fucntion

---
+++ student.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream>
+using namespace std;  
+
+string database="student.csv";
+fstream fout;
+int age;
+string name,place,gender;
+
+void registeration()
+{
+    cout<<"\n-----------------------\n";
+    cout<<"\nREGISTRATION PROCESS\n";
+    cout<<"\nComplete the registration\n-------------------\n";
+    cout<<"\nEnter Name: \n";
+    cin>>name;
+    cout<<"\nEnter Age: \n";
+    cin>>age;
+    cout<<"\nEnter Place: \n";
+    cin>>place;
+    cout<<"\nEnter gender: \n"; 
+    cin>>gender;
+    verification();
+}
+
+int main()
+{
+    cout<<"Student Admission Process\n";
+    registeration();
+    cout<<"End of Student Admission Process\n";
+}
